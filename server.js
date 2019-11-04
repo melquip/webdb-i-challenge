ff3f2c6c5f7d1d07e73c2286977943e07360cb67
Melquisedeque Pereira <melqui1@live.com.pt>
2019-11-04 13:34:08 +0000

    Stretch: Added query params to get accounts

--- server.js
+++ server.js
@@ -6,31 +6,32 @@ const server = express();
 
 server.use(express.json());
 
-server.get('/', async (req, res) => {
-  const result = await db('accounts');
+server.get('/api/accounts', async (req, res) => {
+  const { limit, sortby, sortdir } = req.query;
+  const result = await db('accounts').orderBy(sortby || 'id', sortdir || 'asc').limit(limit || 10000);
   res.status(200).json(result);
 });
 
-server.get('/:id', async (req, res) => {
+server.get('/api/accounts/:id', async (req, res) => {
   const { id } = req.params;
   const result = await db('accounts').where({ id });
   res.status(200).json(result);
 });
 
-server.post('/', async (req, res) => {
+server.post('/api/accounts', async (req, res) => {
   const { name, budget } = req.body;
   const result = await db('accounts').insert({ name, budget });
   res.status(200).json(result);
 });
 
-server.put('/:id', async (req, res) => {
+server.put('/api/accounts/:id', async (req, res) => {
   const { id } = req.params;
   const { name, budget } = req.body;
   const result = await db('accounts').where({ id }).update({ name, budget });
   res.status(200).json(result);
 });
 
-server.delete('/:id', async (req, res) => {
+server.delete('/api/accounts/:id', async (req, res) => {
   const { id } = req.params;
   const result = await db('accounts').where({ id }).del();
   res.status(200).json(result);
